7154258c23ad58405585b35f08e867f9e62d3df3
KhalidGhani333 <khalidghani.333@gmail.com>
2024-11-02 12:20:52 +0500

    push code

---
+++ giaic-final-assignment/index.js
@@ -0,0 +1,14 @@
+
+
+const hamburger = document.getElementById("hamburger");
+const topLinelist = document.getElementById("toplineRightside");
+const navhamburger =document.getElementById("navhamburger")
+const navbarList =document.getElementById("navbarList");
+
+hamburger.addEventListener('click', () => {
+    topLinelist.classList.toggle('active');
+  });
+
+  navhamburger.addEventListener("click",()=>{
+    navbarList.classList.toggle('active');
+  })
